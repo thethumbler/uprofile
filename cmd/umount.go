e3b520fce893591d1744556d7810af69e719dbe6
Mohamed Hamza <manwar@aquilaos.com>
2021-04-04 10:07:19 +0200

    add umount all option

--- cmd/umount.go
+++ cmd/umount.go
@@ -7,16 +7,27 @@ import (
 	"github.com/thethumbler/uprofile/internal"
 )
 
+var unmountAll bool = false
+
 var UmountCommand = &cobra.Command{
-	Use:   "umount <profile>",
+	Use:   "umount [profile]...",
 	Short: "unmount profile",
-	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		context := internal.GetContext()
-		profile := args[0]
+		unmountProfiles := args
 
-		if err := internal.UmountProfile(&context, profile); err != nil {
-			log.Fatal(err)
+		if unmountAll {
+			unmountProfiles = internal.MountedProfiles(&context)
+		}
+
+		for _, profile := range unmountProfiles {
+			if err := internal.UmountProfile(&context, profile); err != nil {
+				log.Fatal(err)
+			}
 		}
 	},
+}
+
+func init() {
+	UmountCommand.PersistentFlags().BoolVarP(&unmountAll, "all", "a", false, "umount all mounted profiles")
 }
